25d764b68005a05bdee6bbfa3e7d456bdc854629
Ricardo <ricardo.machado.nwi@gmail.com>
2022-03-25 13:55:59 -0300

    Main Page BG

--- components/MainPage/index.tsx
+++ components/MainPage/index.tsx
@@ -1,12 +1,14 @@
 import { useSelector } from 'react-redux'
+import { Paper } from '@mui/material'
+import * as S from './styles'
 import type { RootState } from '../../redux/store'
 
 export function MainPage() {
   const userName = useSelector((state: RootState) => state.username.name)
 
   return (
-    <div>
-      <h2>{userName}</h2>
-    </div>
+    <S.MainPageContainer>
+      <Paper>{userName}</Paper>
+    </S.MainPageContainer>
   )
 }
---
+++ components/MainPage/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const MainPageContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+
+  /* background-color: white; */
+`
